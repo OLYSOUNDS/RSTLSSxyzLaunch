e54d82b8701b0986742bf28a3f8212f62f96d351
OLYSOUNDS <liannasylvan@gmail.com>
2022-12-17 14:31:57 -0600

    added playcanvas files to repository github

---
+++ Github Chaweon Hosting files/__settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1618256.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 115513902, 115513910, 115513912, 115513926, 115513929, 115513936 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/115513895/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/115513896/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/115513894/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/115513899/1/basis.wasm.js', 'wasmUrl' : 'files/assets/115513900/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/115513898/1/basis.js', 'preload' : false},
+];
